83e783fa3ae98915e7be42527312b2a14302b4c3
Tarun Jangid <tarunjangid19102002@gmail.com>
2023-08-22 18:47:29 +0530

    1847

--- helpful-maths.cc
+++ helpful-maths.cc
@@ -6,11 +6,12 @@ int main()
     string s;
     cin >> s;
     vector<int> arr;
-    for (int i = 0; i < s.length(); i++)
-        if (s[i] != '+')
-            arr[i] = s[i]-'0';
+    for (int i = 0; i < s.length(); i += 2)
+        arr.push_back(s[i] - '0');
 
-    for (auto val : s)
-        cout << val << " ";
-    cout << endl;
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < s.length(); i += 2)
+        s[i] = arr[i / 2] + '0';
+    cout << s << endl;
 }
