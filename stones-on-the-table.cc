c85e6aa5c9e7e5320ab1629545b61b6ae1360413
tarun jangid <arunjangid19102002@gmail.com>
2023-10-11 11:38:47 +0530

    cfgvkhb

--- stones-on-the-table.cc
+++ stones-on-the-table.cc
@@ -1,4 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {}
+int main()
+{
+    int n;
+    cin >> n;
+    char lastch = '0';
+    int ans = 0;
+    while (n--)
+    {
+        char tmp;
+        cin >> tmp;
+        if (tmp == lastch)
+        {
+            ans++;
+        }
+        lastch = tmp;
+    }
+    cout << ans << endl;
+}
