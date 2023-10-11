26f376c2246ed690d478aa9e7a8eda7db07fddbe
tarun jangid <arunjangid19102002@gmail.com>
2023-10-11 11:30:01 +0530

    long time no see

--- boy-or-girl.cc
+++ boy-or-girl.cc
@@ -1,6 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    string s;
+    cin >> s;
+    bool hash[26] = {false};
+    int nodds = 0;
+    for (char c : s)
+    {
+        if (!hash[c - 'a'])
+            nodds++;
+        hash[c - 'a'] = true;
+    }
+    if (nodds % 2)
+        cout << "IGNORE HIM!" << endl;
+    else
+        cout << "CHAT WITH HER!" << endl;
 }
